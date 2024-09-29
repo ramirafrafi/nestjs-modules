df2ce69dd25d8ec399bc6d5bb578d2fbc73471f3
Rami Rafrafi <ramy.rafrafi@gmail.com>
2024-09-29 14:42:06 +0100

    [nestjs-casl] `CaslPermissions` decorator

--- packages/nestjs-casl/test/controllers/test.controller.ts
+++ packages/nestjs-casl/test/controllers/test.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, UseGuards } from "@nestjs/common";
 import { CaslPoliciesGuard } from "../../src/lib/guards/casl-policies.guard";
 import { CaslPolicies } from "../../src/lib/decorators/casl-policies.decorator";
 import { TestAction } from "../ability/test.action";
+import { CaslPermissions } from "../../src/lib/decorators/casl-permissions.decorator";
 
 @Controller()
 export class TestController {
     @UseGuards(CaslPoliciesGuard)
     @CaslPolicies((ability) => ability.can(TestAction.Allow))
-    @Get('allow')
-    allow() {
+    @Get('casl-policies/allow')
+    allowPolicy() {
         return {
             success: true,
             message: 'OK',
@@ -17,8 +18,38 @@ export class TestController {
 
     @UseGuards(CaslPoliciesGuard)
     @CaslPolicies((ability) => ability.can(TestAction.Deny))
-    @Get('deny')
-    deny() {
+    @Get('casl-policies/deny')
+    denyPolicy() {
+        return {
+            success: true,
+            message: 'OK',
+        };
+    }
+
+    @UseGuards(CaslPoliciesGuard)
+    @CaslPermissions(TestAction.Allow)
+    @Get('casl-permissions/allow')
+    allowPermission() {
+        return {
+            success: true,
+            message: 'OK',
+        };
+    }
+
+    @UseGuards(CaslPoliciesGuard)
+    @CaslPermissions(TestAction.Deny)
+    @Get('casl-permissions/deny')
+    denyPermission() {
+        return {
+            success: true,
+            message: 'OK',
+        };
+    }
+
+    @UseGuards(CaslPoliciesGuard)
+    @CaslPermissions(TestAction.Allow, TestAction.Deny)
+    @Get('casl-permissions/multi/deny')
+    denyMultiPermissions() {
         return {
             success: true,
             message: 'OK',
