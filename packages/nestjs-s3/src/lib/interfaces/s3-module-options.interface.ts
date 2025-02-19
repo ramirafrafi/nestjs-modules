3da83b64a855b65b0eb2c022118f55786c110507
Rami Rafrafi <ramy.rafrafi@gmail.com>
2025-02-19 11:58:18 +0100

    Remove package `nestjs-s3`

--- packages/nestjs-s3/src/lib/interfaces/s3-module-options.interface.ts
+++
@@ -1,5 +0,0 @@
-import { S3ClientConfig } from "@aws-sdk/client-s3";
-
-export interface IS3ModuleOptions extends S3ClientConfig {
-  endpoint?: string;
-}
--- packages/nestjs-s3/src/lib/s3.tokens.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from "@nestjs/common";
-
-export const S3_OPTIONS: InjectionToken = Symbol('S3_OPTIONS');
-
-export const S3_SERVICE: InjectionToken = Symbol('S3_SERVICE');
