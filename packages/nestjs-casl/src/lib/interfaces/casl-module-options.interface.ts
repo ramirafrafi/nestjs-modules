d331c97994b95c0c13321b351586b522a70f83c0
Rami Rafrafi <ramy.rafrafi@gmail.com>
2025-02-19 14:39:10 +0100

    [nestjs-casl] Refactor

--- packages/nestjs-casl/src/lib/interfaces/casl-module-options.interface.ts
+++ packages/nestjs-casl/src/lib/interfaces/casl-module-options.interface.ts
@@ -1,9 +1,15 @@
-import { Abilities } from "@casl/ability";
-import { ICaslAbilityFactory } from "./casl-ability-factory.interface";
-import { DynamicModule, Type } from "@nestjs/common";
+import { Abilities } from '@casl/ability';
+import { DynamicModule, FactoryProvider, Type } from '@nestjs/common';
+import { ICaslAbilityFactory } from './casl-ability-factory.interface';
 
 type BaseOptions = Pick<DynamicModule, 'global'>;
 
 export interface ICaslModuleOptions<T extends Abilities> extends BaseOptions {
-    abilityFactory: ICaslAbilityFactory<T> | Type<ICaslAbilityFactory<T>>;
+  abilityFactory: ICaslAbilityFactory<T> | Type<ICaslAbilityFactory<T>>;
 }
+
+export type ICaslModuleAsyncOptions<T extends Abilities> = Pick<
+  DynamicModule,
+  'global' | 'imports'
+> &
+  Pick<FactoryProvider<ICaslAbilityFactory<T>>, 'inject' | 'useFactory'>;
