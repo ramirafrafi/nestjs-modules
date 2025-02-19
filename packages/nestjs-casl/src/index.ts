cd00c700f27e4bf6bdc6eafec733a61d7c1e5c59
Rami Rafrafi <ramy.rafrafi@gmail.com>
2025-02-19 14:39:10 +0100

    [nestjs-casl] Refactor

--- packages/nestjs-casl/src/index.ts
+++ packages/nestjs-casl/src/index.ts
@@ -4,7 +4,6 @@ export * from './lib/decorators/casl-policies.decorator';
 export * from './lib/decorators/casl-subject.decorator';
 export * from './lib/guards/casl.guard';
 export * from './lib/interfaces/casl-ability-factory.interface';
-export * from './lib/interfaces/casl-module-async-options.interface';
 export * from './lib/interfaces/casl-module-options.interface';
 export * from './lib/interfaces/casl-policy-class.interface';
 export * from './lib/interfaces/casl-policy-handler.interface';
@@ -12,3 +11,4 @@ export * from './lib/types/can-rule.type';
 export * from './lib/types/cannot-rule.type';
 export * from './lib/types/casl-ability.type';
 export * from './lib/types/casl-policy.type';
+
--- packages/nestjs-casl/src/lib/casl.module.ts
+++ packages/nestjs-casl/src/lib/casl.module.ts
@@ -2,42 +2,44 @@ import type { Abilities } from '@casl/ability';
 import type { DynamicModule, Type } from '@nestjs/common';
 import { Module, Scope } from '@nestjs/common';
 import { CASL_ABILITY_FACTORY } from './interfaces/casl-ability-factory.interface';
-import { ICaslModuleAsyncOptions } from './interfaces/casl-module-async-options.interface';
-import { ICaslModuleOptions } from './interfaces/casl-module-options.interface';
+import {
+  ICaslModuleAsyncOptions,
+  ICaslModuleOptions,
+} from './interfaces/casl-module-options.interface';
 
 @Module({})
 export class CaslModule {
   static forRoot<T extends Abilities>(options: ICaslModuleOptions<T>): DynamicModule {
     return {
-      global: options.global,
       module: CaslModule,
+      global: options.global,
       providers: [
-        (options.abilityFactory as Type).name
+        (options.options.abilityFactory as Type).name
           ? {
-            provide: CASL_ABILITY_FACTORY,
-            useClass: options.abilityFactory as Type,
-            scope: Scope.REQUEST,
-          }
+              provide: CASL_ABILITY_FACTORY,
+              useClass: options.options.abilityFactory as Type,
+              scope: Scope.REQUEST,
+            }
           : {
-            provide: CASL_ABILITY_FACTORY,
-            useValue: options.abilityFactory,
-            scope: Scope.REQUEST,
-          },
+              provide: CASL_ABILITY_FACTORY,
+              useValue: options.options.abilityFactory,
+              scope: Scope.REQUEST,
+            },
       ],
       exports: [CASL_ABILITY_FACTORY],
     };
   }
 
-  static forRootAsync<T extends Abilities>(asyncOptions: ICaslModuleAsyncOptions<T>): DynamicModule {
+  static forRootAsync<T extends Abilities>(options: ICaslModuleAsyncOptions<T>): DynamicModule {
     return {
-      global: asyncOptions.global,
       module: CaslModule,
-      imports: asyncOptions.imports,
+      global: options.global,
+      imports: options.imports,
       providers: [
         {
           provide: CASL_ABILITY_FACTORY,
-          useFactory: asyncOptions.useFactory,
-          inject: asyncOptions.inject,
+          useFactory: async (...args) => (await options.useFactory(args)).abilityFactory,
+          inject: options.inject,
           scope: Scope.REQUEST,
         },
       ],
